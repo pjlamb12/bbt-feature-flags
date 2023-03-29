a7955305c78394adf716fac016b9231d4d57abe1
Preston Lamb <pjlamb12@prestons-mbp.lan>
2023-03-29 00:48:48 -0600

    added the directive to the sale banner

--- libs/home/src/lib/home.module.ts
+++ libs/home/src/lib/home.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { homeRoutes } from './lib.routes';
 import { HomeComponent } from './home/home.component';
 import { SalePageComponent } from './sale-page/sale-page.component';
+import { FeatureFlagsModule } from '@bbt-feature-flags/feature-flags';
 
 @NgModule({
-	imports: [CommonModule, RouterModule.forChild(homeRoutes)],
+	imports: [CommonModule, RouterModule.forChild(homeRoutes), FeatureFlagsModule],
 	declarations: [HomeComponent, SalePageComponent],
 })
 export class HomeModule {}
