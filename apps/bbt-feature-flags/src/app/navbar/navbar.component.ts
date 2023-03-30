1fd16028f026786fc759d4492990ced35f0f6266
Preston Lamb <pjlamb12@prestons-mbp.lan>
2023-03-29 22:10:05 -0600

    using the service to check if a flag is enabled

--- apps/bbt-feature-flags/src/app/navbar/navbar.component.ts
+++ apps/bbt-feature-flags/src/app/navbar/navbar.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { FeatureFlags, FeatureFlagsService } from '@bbt-feature-flags/feature-flags';
+import { map } from 'rxjs';
 
 @Component({
 	selector: 'bbt-navbar',
 	templateUrl: './navbar.component.html',
 	styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+	public links$ = this._flags.featureFlags$.pipe(
+		map((flags: FeatureFlags) => {
+			const links = [
+				{
+					label: 'Home',
+					path: '/',
+				},
+			];
+
+			if (this._flags.isFeatureEnabled('saleActive')) {
+				links.push({
+					label: 'Sale',
+					path: '/sale',
+				});
+			}
+
+			return links;
+		}),
+	);
+
+	constructor(private _flags: FeatureFlagsService) {}
+}
